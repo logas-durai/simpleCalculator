aa5f8ff4d2e7b8925cc38c3ead1eb62bd67fdbc7
logas-durai <durai@logasapp.com>
2020-05-10 22:56:31 +0530

    error come

--- App.js
+++ App.js
@@ -2,130 +2,149 @@ import React from 'react';
 import { StyleSheet, Text, View, Button, TextInput, TouchableOpacity } from 'react-native';
 import { Logs } from 'expo';
 import { Colors } from 'react-native/Libraries/NewAppScreen';
-
-var addition = '+', subtraction = '-', multiplication = '*', divide = '/', btnEqual = '=', btnClear = 'clear';
-var btnZero = 'zero', btnOne = 'one', btnTwo = 'two', btnThree = 'three', btnFour = 'four', btnFive = 'five', btnSix = 'six', btnSeven = 'seven', btnEight = 'eight', btnNine = 'nine';
-var FirstRowArray = ["1", "2", "3"];
-var SecondRowArray = ['4', '5', '6'];
-var ThirdRowArray = ['7', '8', '9'];
-var FourthRowArray = ['clear', '0'];
+var dataArray = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
+var operatorArray = ['+', '-', '*'];
+var bottomArray = ['clear', '0', '/', '='];
 export default class App extends React.Component {
 
   state = {
-    value: '0',
+    inputValue: '0',
     resultValue: '0',
-    type: ''
+    type: '',
+    tempValue: '0'
   }
 
+  storedValue(data) {
 
-  storeData(data) {
-    let { value } = this.state;
-    if (data != 'clear') {
-      value = value + data;
-    } else {
-      value = '';
+    let { inputValue, resultValue, type, tempValue } = this.state;
+
+    inputValue = (data != 'clear') ? inputValue + data : '0';
+    tempValue = (data != 'clear') ? tempValue + data : '0';
+    if (data == 'clear') {
+      resultValue = '0';
     }
-    this.setState({ value });
+    this.setState({ inputValue, tempValue });
+    console.log("Input Value = ", inputValue + "  Temp Value= " + tempValue);
+
   }
 
   operationData(data) {
-    let { value, resultValue, type } = this.state;
+
+    let { type, inputValue, resultValue, tempValue } = this.state;
+    console.log("DATA", data);
     type = data;
-    resultValue = value;
-    value = '';
-    this.setState({ value });
-    this.setState({ type })
-    this.setState({ resultValue });
-    console.log(value + "=" + type + "=" + resultValue);
+    resultValue = inputValue;
+    inputValue = '0';
+    tempValue = tempValue + data;
+    this.setState({ type, inputValue, resultValue, tempValue })
+    this.calculationFun(inputValue, resultValue, type);
+    console.log("OPERATION FUNCTION EXE ===", "Input Value = ", inputValue + "  Temp Value= " + tempValue + "  RESULT VALUE= " + resultValue + " TYPE= " + type);
   }
 
-  calculationData(data1, data2, operationType) {
-    console.log("DATA1= " + data1 + " DATA2= " + data2 + " opetation= " + operationType);
-    let { value, resultValue } = this.state;
+  otherData(data) {
+
+    let { type, inputValue, resultValue } = this.state;
+
+    if (data == 'clear') {
+      this.storedValue(data);
+    } else if (data == '0') {
+      this.storedValue(data);
+    } else if (data == '/') {
+      operationData(data)
+    } else if (data == '=') {
+      calculationFun(inputValue, resultValue, type);
+    }
+
+  }
+
+  calculationFun(firstData, secondData, operationType) {
+    let { inputValue, resultValue, tempValue } = this.state;
     if (operationType == '+') {
-      resultValue = parseInt(data2) + parseInt(data1);
+      resultValue = parseInt(firstData) + parseInt(secondData);
     } else if (operationType == '-') {
-      resultValue = parseInt(data1) - parseInt(data2);
+      resultValue = parseInt(firstData) - parseInt(secondData);
     } else if (operationType == '*') {
-      resultValue = parseInt(data1) * parseInt(data2);
+      resultValue = parseInt(firstData) * parseInt(secondData);
     } else if (operationType == '/') {
-      // resultValue = parseInt(data1) / parseInt(data2);
-      resultValue = 12 / 6;
+      resultValue = parseInt(firstData) / parseInt(secondData);
     } else {
       resultValue = 'Error'
     }
+    inputValue = '0';
+    console.log("DATASSSSSS", inputValue + "===" + resultValue + "===");
+    this.setState({ tempValue, resultValue, inputValue });
 
-    console.log(typeof (resultValue));
-    value = ''
-    this.setState({ value });
-    this.setState({ resultValue });
 
   }
 
 
+
   render() {
-    const { value, resultValue, type } = this.state;
+    const { inputValue, resultValue, type, tempValue } = this.state;
     return (<View style={viewStyles.mainStyle}>
       <View style={viewStyles.resultContainer}>
-
         <Text style={viewStyles.textOutputValue}>{resultValue}</Text>
-        <Text style={viewStyles.textInputValue}>{value}</Text>
+        <Text style={viewStyles.textInputValue}>{tempValue}</Text>
       </View>
-      <View style={viewStyles.inputContainer}>
 
-        <View style={{ flex: 1 }}>
-          <View style={{ flex: 1, flexDirection: 'row' }}>
+      <View style={viewStyles.keyboardContainer}>
 
-            {FirstRowArray.map((arrValue, key) => (
-              <Text key={key} style={viewStyles.textStyle} onPress={() => this.storeData(arrValue)}>{arrValue}</Text>))}
 
-            <Text onPress={() => this.operationData('+')} style={viewStyles.textStyle}>+</Text>
+        <View style={viewStyles.subContainer}>
 
+          <View style={viewStyles.numContainer}>
+            {dataArray.map((arr, key) =>
+              <Text key={key} style={viewStyles.valueContainer} onPress={() => this.storedValue(arr)}>{arr}</Text>)}
           </View>
-          <View style={{ flex: 1, flexDirection: 'row' }}>
-            {SecondRowArray.map((item, key1) => (
-              <Text key={key1} style={viewStyles.textStyle} onPress={() => this.storeData(item)}>{item}</Text>))}
-
-            <Text onPress={() => this.operationData('-')} style={viewStyles.textStyle}>-</Text>
+          <View style={viewStyles.operationContainer}>
+            {operatorArray.map((arr, key) =>
+              <Text key={key} style={viewStyles.valuesContainer} onPress={() => this.operationData(arr)}>{arr}</Text>)}
           </View>
 
-          <View style={{ flex: 1, flexDirection: 'row' }}>
-            {ThirdRowArray.map((item, key2) => (<Text key={key2} style={viewStyles.textStyle} onPress={() => this.storeData(item)}>{item}</Text>))}
-            <Text onPress={() => this.operationData('*')} style={viewStyles.textStyle}>*</Text>
-          </View>
-          <View style={{ flex: 1, flexDirection: 'row' }}>
-            {FourthRowArray.map((item, key4) => (<Text key={key4} style={viewStyles.textStyle} onPress={() => this.storeData(item)}>{item}</Text>))}
-            <Text onPress={() => this.operationData('/')} style={viewStyles.textStyle}>/</Text>
-            <Text onPress={() => this.calculationData(value, resultValue, type)} style={viewStyles.textStyle}>=</Text>
-          </View>
+
 
         </View>
 
+
+
+        <View style={viewStyles.sub2Container}>
+
+
+          <View style={viewStyles.operationsContainer}>
+            {bottomArray.map((arr, key) =>
+              <Text key={key} style={viewStyles.valuessContainer} onPress={() => this.otherData(resultValue, value, arr)}>{arr}</Text>)}
+          </View>
+
+
+
+        </View>
+
+
       </View>
-    </View>);
+
+    </View >);
   }
 }
 
 
-const viewStyles = new StyleSheet.create({
+const viewStyles = StyleSheet.create({
   mainStyle: {
-    flex: 1,
+    flex: 10,
     padding: 5,
     backgroundColor: '#9f93e3'
   },
 
   resultContainer: {
 
-    flex: 1,
+    flex: 3,
     backgroundColor: '#fff',
     borderRadius: 5,
     marginTop: 30,
     marginBottom: 10,
     borderColor: '#000',
     borderWidth: 2
-  },
-  textInputValue: {
-    fontSize: 30,
+  }, textInputValue: {
+    fontSize: 25,
     flex: 2,
     color: '#000000',
     alignSelf: 'flex-end',
@@ -135,26 +154,80 @@ const viewStyles = new StyleSheet.create({
     fontSize: 30,
     flex: 1,
     color: '#000000',
-    alignSelf: 'flex-end'
+    alignSelf: 'flex-end',
+    fontWeight: "bold"
   },
-  inputContainer: {
-    flex: 9,
-    flex: 3
+  keyboardContainer: {
+    flex: 7,
+    backgroundColor: '#fff',
   },
-  textStyle: {
-    fontSize: 20,
+
+  subContainer: {
     flex: 1,
-    color: '#ffffff',
-    textAlign: 'center',
-    margin: 5,
-    textAlignVertical: 'center',
-    backgroundColor: 'blue'
+    flexDirection: 'row',
+  },
+  sub2Container: {
+    flex: 1.7,
+    flexDirection: 'row',
+  },
+  operationsContainer: {
+    flex: 1,
+    backgroundColor: "#bababa",
+    flexWrap: 'wrap',
+    flexDirection: 'row',
+
+
   },
 
-  buttonStyle: {
-    padding: 10,
-    backgroundColor: '#202646',
-    borderRadius: 5
-  }
+  numContainer: {
+    flex: 7.5,
+    backgroundColor: "#bababa",
+    flexDirection: 'row',
+    flexWrap: 'wrap',
 
+
+  },
+  operationContainer: {
+    flex: 2.5,
+    backgroundColor: "#bababa",
+    flexWrap: 'wrap',
+    flexDirection: 'row',
+
+
+  },
+
+  valueContainer: {
+    textAlign: 'center',
+    color: '#fff',
+    fontSize: 20,
+    backgroundColor: '#23432e',
+    padding: 5,
+    margin: 5,
+    flexWrap: 'wrap',
+    flexDirection: 'row',
+    width: '29%'
+  },
+
+  valuesContainer: {
+    textAlign: 'center',
+    color: '#fff',
+    fontSize: 20,
+    backgroundColor: '#23432e',
+    padding: 5,
+    margin: 5,
+    flexWrap: 'wrap',
+    width: '90%'
+  },
+
+  valuessContainer: {
+    textAlign: 'center',
+    color: '#fff',
+    fontSize: 20,
+    backgroundColor: '#23432e',
+    padding: 5,
+    margin: 5,
+    flexWrap: 'wrap',
+    width: '22%',
+    flexDirection: 'row'
+  },
 }) 
